828d11a48252834a107cbe4afe0121d58f8e821f
TheRealMattHUN <legolegend007@gmail.com>
2024-06-11 10:04:15 +0200

    removed hash link

--- src/routes/pages/layout.tsx
+++ src/routes/pages/layout.tsx
@@ -1,22 +1,16 @@
-import {Box, Center, Text} from "@mantine/core";
+import {Flex, Text} from "@mantine/core";
 import {Suspense} from "react";
 import {Outlet} from "react-router";
 import {Link} from "react-router-dom";
-import { HashLink } from 'react-router-hash-link';
 function Layout() {
     return (
         <Suspense fallback="loading!">
-            <Center>
-                <Box>
-                    <Text component={HashLink} to="/#">Home</Text>
-                    <Text component={HashLink} to="/#about">About me</Text>
-                    <Text component={HashLink} to="/#lang">Languages</Text>
-                    <Text component={Link} to="/projects">Projects</Text>
-                </Box>
-            </Center>
+            <Flex gap="md" justify="center" align="center">
+                <Text component={Link} to="/">Home</Text>
+                <Text component={Link} to="/projects">Projects</Text>
+            </Flex>
             <Outlet />
         </Suspense>
-
     )
 }
 export default Layout
